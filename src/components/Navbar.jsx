b7e4bd74898e3cc792d73281fb2bddf7cec67162
Nghia <nghiavu144@gmail.com>
2024-12-04 21:23:29 -0600

    Feature section with titled effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,6 +68,7 @@ const Navbar = () => {
         <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
             <header className='absolute top-1/2 w-full -translate-y-1/2'>
                 <nav className='flex size-full items-center justify-between p-4'>
+                    {/* Logo and Product button */}
                     <div className='flex items-center gap-7'>
                         <img src='/img/logo.png' alt='logo' className='w-10' />
                         <Button 
@@ -77,10 +78,12 @@ const Navbar = () => {
                             containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
                         />
                     </div>
+
+                    {/* Navigation Links and Audio Button */}
                     <div className='flex h-full items-center'>
                         <div className='hidden md:block'>
-                            {navItems.map((item) => (
-                                <a key={item} href={`#${item.toLowerCase()}`} className='nav-hover-btn'>
+                            {navItems.map((item, index) => (
+                                <a key={index} href={`#${item.toLowerCase()}`} className='nav-hover-btn'>
                                     {item}
                                 </a>
                             ))}
@@ -89,7 +92,7 @@ const Navbar = () => {
                         <button onClick={toggleAudioIndicator} className='ml-10 flex items-center space-x-0.5'>
                             <audio ref={audioElementRef} className='hidden' src='/audio/loop.mp3' loop />
                                 {[1, 2, 3, 4].map((bar) => (
-                                    <div key={bar} className={`indicator-line ${isIndicatorActive ? 'active' : ''}`} style={{ animationDelay: `${bar * 0.1}`}}>
+                                    <div key={bar} className={`indicator-line ${isIndicatorActive ? 'active' : ''}`} style={{ animationDelay: `${bar * 0.1}s`}}>
 
                                     </div>
                                 ))}
